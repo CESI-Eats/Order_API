d0f867da99900074eefb3114fe499de5d9d68e52
KikosS41 <killian.legendre41@gmail.com>
2023-06-26 15:48:58 +0200

    struct done

--- src/lapinou.ts
+++ src/lapinou.ts
@@ -1,11 +1,13 @@
-import { connectLapinou } from './services/lapinouService';
-import { createOrderingExchange } from './exchanges/orderingExchange';
-import { createOrderExchange } from './exchanges/orderExchange';
+import {connectLapinou} from './services/lapinouService';
+import {createOrderingExchange} from './exchanges/orderingExchange';
+import {createOrderExchange} from './exchanges/orderExchange';
+import {getHistoricExchange} from "./exchanges/historicExchange";
 
-export function initLapinou(){
+export function initLapinou() {
     connectLapinou().then(async () => {
-      createOrderingExchange();
-      createOrderExchange();
+        createOrderingExchange();
+        createOrderExchange();
+        getHistoricExchange();
     }).catch((err) => {
         console.error('Failed to connect to rabbitMQ');
     });
