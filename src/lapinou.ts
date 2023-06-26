1c430f97ede633a478dbb27c8c036423adbd0202
Helrho <armand-gab@hotmail.fr>
2023-06-26 15:00:59 +0200

    [ADD] orderExchange

--- src/lapinou.ts
+++ src/lapinou.ts
@@ -1,9 +1,11 @@
 import { connectLapinou } from './services/lapinouService';
 import { createOrderingExchange } from './exchanges/orderingExchange';
+import { createOrderExchange } from './exchanges/orderExchange';
 
 export function initLapinou(){
     connectLapinou().then(async () => {
       createOrderingExchange();
+      createOrderExchange();
     }).catch((err) => {
         console.error('Failed to connect to rabbitMQ');
     });
