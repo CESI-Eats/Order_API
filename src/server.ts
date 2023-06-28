26f79723956c46b9a94a2d1fbba76f5f5c55d9b0
Armand DEBESSE <73818074+armanddebesse@users.noreply.github.com>
2023-06-28 22:38:04 +0200

    Release/0.2.0 (#12)
    * Develop (#11)
    
    * Release/0.2.0 (#10)
    
    * add ci tools and release
    
    * Delete Order.ts
    
    * Update server.ts
    
    * Update lapinou.ts
    
    ---------
    
    Co-authored-by: FW11-GMIOT/gmiot001 <guillaumemiot.gm@gmail.com>
    Co-authored-by: Guillaume Miot <73818815+Guieme@users.noreply.github.com>
    
    * Update server.ts
    
    * Update lapinou.ts
    
    ---------
    
    Co-authored-by: FW11-GMIOT/gmiot001 <guillaumemiot.gm@gmail.com>
    Co-authored-by: Guillaume Miot <73818815+Guieme@users.noreply.github.com>
    
    * Update server.ts
    
    ---------
    
    Co-authored-by: FW11-GMIOT/gmiot001 <guillaumemiot.gm@gmail.com>
    Co-authored-by: Guillaume Miot <73818815+Guieme@users.noreply.github.com>

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import { initLapinou } from './lapinou';
 
 mongoose.connect(process.env.MONGODB_URI as string, { useNewUrlParser: true, useUnifiedTopology: true } as any)
-  .then(() => console.log('Successfully connected to MongoDB.'));
+  .then(() => console.log('Successfully connected to MongoDB.'))
+  .catch((error) => console.log('Failed to connect to MongoDB.', error));
 
 initLapinou();
