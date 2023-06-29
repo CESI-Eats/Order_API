3d0d3fe35cd0e0e9e61344a3f15eb6b81d9b159a
KikosS41 <killian.legendre41@gmail.com>
2023-06-29 03:49:39 +0200

    updates

--- src/exchanges/historicExchange.ts
+++ src/exchanges/historicExchange.ts
@@ -3,6 +3,39 @@ import Order from "../models/order";
 
 export function getHistoricExchange() {
     initExchange('historic').then(exchange => {
+        initQueue(exchange, 'get.orders.for.user').then(({queue, topic}) => {
+            handleTopic(queue, topic, async (msg) => {
+                const message = msg.content as MessageLapinou;
+                try {
+                    console.log(` [x] Received message: ${JSON.stringify(message)}`);
+                    const orders = await Order.find({_idUser: message.content.id}).lean();
+
+                    console.log(`Orders retrieved : ${orders}`);
+
+                    await sendMessage(
+                        {
+                            success: true,
+                            content: orders,
+                            correlationId: message.correlationId,
+                            sender: 'order',
+                        },
+                        message.replyTo ?? ''
+                    );
+                } catch (err) {
+                    const errMessage = err instanceof Error ? err.message : 'An error occurred';
+                    await sendMessage(
+                        {
+                            success: false,
+                            content: errMessage,
+                            correlationId: message.correlationId,
+                            sender: 'order',
+                        },
+                        message.replyTo ?? ''
+                    );
+                }
+            });
+        });
+
         initQueue(exchange, 'get.orders.for.deliveryman').then(({queue, topic}) => {
             handleTopic(queue, topic, async (msg) => {
                 const message = msg.content as MessageLapinou;
@@ -13,9 +46,6 @@ export function getHistoricExchange() {
                     });
 
                     console.log(`Orders retrieved : ${orders}`);
-                    if (orders.length === 0) {
-                        throw new Error('Orders not found');
-                    }
 
                     await sendMessage({
                         success: true,
