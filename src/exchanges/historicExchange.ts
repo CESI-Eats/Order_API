698a801318e0c5949cc63bf2f8df49dc8618064a
KikosS41 <killian.legendre41@gmail.com>
2023-06-30 03:56:08 +0200

    added

--- src/exchanges/historicExchange.ts
+++ src/exchanges/historicExchange.ts
@@ -59,5 +59,31 @@ export function getHistoricExchange() {
                 }
             });
         });
+
+        initQueue(exchange, 'get.orders.for.restorer').then(({queue, topic}) => {
+            handleTopic(queue, topic, async (msg) => {
+                const message = msg.content as MessageLapinou;
+                try {
+                    console.log(` [x] Received message: ${JSON.stringify(message)}`);
+
+                    const orders = await Order.find({_idRestorer: message.content.id}).lean();
+
+                    await sendMessage({
+                        success: true,
+                        content: orders,
+                        correlationId: message.correlationId,
+                        sender: 'order'
+                    }, message.replyTo ?? '');
+                } catch (err) {
+                    const errMessage = err instanceof Error ? err.message : 'An error occurred';
+                    await sendMessage({
+                        success: false,
+                        content: errMessage,
+                        correlationId: message.correlationId,
+                        sender: 'order'
+                    }, message.replyTo ?? '');
+                }
+            });
+        });
     });
 }
